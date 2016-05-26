422d3b698efe608e30e8396c0b32d153ac2aa7fa
Jim Bennett <jim@jimbobbennett.io>
2016-05-26 12:40:39 +1200

    Added iOS addin

---
+++ MVVMCross.XSAddIn/Templates/Projects/iOS/Views/FirstView.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using MvvmCross.Binding.BindingContext;
+using MvvmCross.iOS.Views;
+using ${ProjectName}.Core.ViewModels;
+
+namespace ${ProjectName}.Views
+{
+    [MvxFromStoryboard]
+    public partial class FirstView : MvxViewController<FirstViewModel>
+    {
+        public FirstView(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            var set = this.CreateBindingSet<FirstView, FirstViewModel>();
+            set.Bind(Label).To(vm => vm.Hello);
+            set.Bind(TextField).To(vm => vm.Hello);
+            set.Apply();
+        }
+    }
+}
