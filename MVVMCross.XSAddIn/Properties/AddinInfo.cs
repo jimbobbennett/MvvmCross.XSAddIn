e8b8c2c108ada5670c80cc42f5d45bd89fac3758
Jim Bennett <jim@jimbobbennett.io>
2016-06-13 12:35:04 +1200

    Fixing text

--- MVVMCross.XSAddIn/Properties/AddinInfo.cs
+++ MVVMCross.XSAddIn/Properties/AddinInfo.cs
@@ -7,9 +7,9 @@ using Mono.Addins.Description;
     Version = "1.0"
 )]
 
-[assembly: AddinName ("MVVMCross Template pack")]
+[assembly: AddinName ("MvvmCross Template pack")]
 [assembly: AddinCategory ("IDE extensions")]
-[assembly: AddinDescription ("Adds a new project type for creating cross-plaform MVVMCross apps.  The project that is created is" +
-                             "the same as what you would get if you installed the MVVMCross Start Pack nuget and followed the instructions.")]
+[assembly: AddinDescription ("Adds a new project type for creating cross-plaform MvvmCross apps.  The project that is created is " +
+                             "the same as what you would get if you installed the MvvmCross Start Pack nuget and followed the instructions.")]
 [assembly: AddinAuthor ("Jim Bennett")]
 
