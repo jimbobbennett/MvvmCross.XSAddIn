4a67c47bcbf2f48e0a8683fe54d297f1673bf7ec
Jim Bennett <jim@jimbobbennett.io>
2016-06-16 20:10:36 +1200

    Increased MvvmCross version to 4.2

--- MVVMCross.XSAddIn/Properties/AddinInfo.cs
+++ MVVMCross.XSAddIn/Properties/AddinInfo.cs
@@ -4,7 +4,7 @@ using Mono.Addins.Description;
 [assembly: Addin (
     "MVVMCross.XSAddIn",
     Namespace = "MVVMCross.XSAddIn",
-    Version = "1.0.2"
+    Version = "1.0.3"
 )]
 
 [assembly: AddinName ("MvvmCross Template pack")]
