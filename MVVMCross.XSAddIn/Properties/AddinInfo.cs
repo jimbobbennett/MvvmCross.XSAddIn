d5004084c6db0b6d62a8968988970eb953e78ba4
Jim Bennett <jim@jimbobbennett.io>
2016-07-27 20:02:28 +1200

    Upping version

--- MVVMCross.XSAddIn/Properties/AddinInfo.cs
+++ MVVMCross.XSAddIn/Properties/AddinInfo.cs
@@ -4,7 +4,7 @@ using Mono.Addins.Description;
 [assembly: Addin (
     "MVVMCross.XSAddIn",
     Namespace = "MVVMCross.XSAddIn",
-    Version = "1.1.3"
+    Version = "1.1.4"
 )]
 
 [assembly: AddinName ("MvvmCross Template pack")]
