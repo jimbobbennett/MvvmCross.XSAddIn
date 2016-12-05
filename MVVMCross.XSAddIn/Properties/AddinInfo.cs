b5cfc3bb094db8b8a1c7a54f5a94ff4b118d2a97
Jim Bennett <jim@jimbobbennett.io>
2016-12-05 20:22:30 +1300

    Changed profile to 111

--- MVVMCross.XSAddIn/Properties/AddinInfo.cs
+++ MVVMCross.XSAddIn/Properties/AddinInfo.cs
@@ -4,7 +4,7 @@ using Mono.Addins.Description;
 [assembly: Addin (
     "MVVMCross.XSAddIn",
     Namespace = "MVVMCross.XSAddIn",
-    Version = "1.1.9"
+    Version = "1.1.13"
 )]
 
 [assembly: AddinName ("MvvmCross Template pack")]
