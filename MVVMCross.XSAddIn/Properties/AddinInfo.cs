be3d1332252d4fb6c61be97d35863d5d2033f2f7
Jim Bennett <jim@jimbobbennett.io>
2017-01-11 22:19:52 +1300

    Version bump

--- MVVMCross.XSAddIn/Properties/AddinInfo.cs
+++ MVVMCross.XSAddIn/Properties/AddinInfo.cs
@@ -4,7 +4,7 @@ using Mono.Addins.Description;
 [assembly: Addin (
     "MVVMCross.XSAddIn",
     Namespace = "MVVMCross.XSAddIn",
-    Version = "1.3.3"
+    Version = "1.3.4"
 )]
 
 [assembly: AddinName ("MvvmCross Template pack")]
