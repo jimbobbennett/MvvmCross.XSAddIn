fa2317d9cb3d1c3af09e412864b6900bde787401
Jim Bennett <jim@jimbobbennett.io>
2017-07-21 13:04:30 +1200

    Adding note on SDK 7.1

--- MVVMCross.XSAddIn/Properties/AddinInfo.cs
+++ MVVMCross.XSAddIn/Properties/AddinInfo.cs
@@ -12,5 +12,6 @@ using Mono.Addins.Description;
 [assembly: AddinDescription ("Adds 2 new project types for creating cross-plaform MvvmCross apps either using Xamarin.iOS and Xamarin.Android, or using Xamarin.Forms.\n" +
                              "The project that is created for Xamarin iOS and Xamarin.Android is " +
                              "the same as what you would get if you installed the MvvmCross Starter Pack nuget and followed the instructions.\n" +
-                             "The project that is created for Xamarin.Forms is the same as the example from the MvvmCross-Forms repo on GitHub.")]
+                             "The project that is created for Xamarin.Forms is the same as the example from the MvvmCross-Forms repo on GitHub.\n" +
+                             "NOTE - you will need Android SDK 7.1 (API 25) installed for the apps created with this template to work.")]
 [assembly: AddinAuthor ("Jim Bennett")]
