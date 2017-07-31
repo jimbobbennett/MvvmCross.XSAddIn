4c97ff4246fcede9b05720247cdb0fed06b176e4
Jim Bennett <jim@jimbobbennett.io>
2017-07-31 12:41:38 +1200

    tweaks

--- MVVMCross.XSAddIn/Properties/AddinInfo.cs
+++ MVVMCross.XSAddIn/Properties/AddinInfo.cs
@@ -4,7 +4,7 @@ using Mono.Addins.Description;
 [assembly: Addin (
     "MVVMCross.XSAddIn",
     Namespace = "MVVMCross.XSAddIn",
-    Version = "1.4.12"
+    Version = "1.4.13"
 )]
 
 [assembly: AddinName ("MvvmCross Template pack")]
@@ -13,5 +13,5 @@ using Mono.Addins.Description;
                              "The project that is created for Xamarin iOS and Xamarin.Android is " +
                              "the same as what you would get if you installed the MvvmCross Starter Pack nuget and followed the instructions.\n" +
                              "The project that is created for Xamarin.Forms is the same as the example from the MvvmCross-Forms repo on GitHub.\n" +
-                             "NOTE - you will need Android SDK 7.1 (API 25) installed for the apps created with this template to work.")]
+                             "NOTE - you will need Android SDK 7.1 (API 25) or later installed for the apps created with this template to work.")]
 [assembly: AddinAuthor ("Jim Bennett")]
